c57d5f5e0fa91e9a540494d660e623a3aa13c3f4
LayaGhodsi <47304124+LayaGhodsi@users.noreply.github.com>
2024-09-09 18:59:16 -0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -14,7 +14,7 @@ var customScripts = {
                     columnWidth: $container.width() / $resize
                 }
             });
-            $("a[href='#top']").click(function () {
+            $("a[href='top']").click(function () {
                 $("html, body").animate({ scrollTop: 0 }, "slow");
                 return false;
             });
